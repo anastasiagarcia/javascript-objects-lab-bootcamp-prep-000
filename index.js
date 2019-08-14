34dfa775226b5a369bcfcff0bb3d2be6083e35ba
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 16:39:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ var recipes={
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key]=value;
-  return object;
+ object.assign({},object,{key:[value]});
+ return object;
 }
 
-// function updateObjectWithKeyAndValue(object, key,value){
-//   object.assign({},object,{key:[value]});
-//   return object;
-// }
