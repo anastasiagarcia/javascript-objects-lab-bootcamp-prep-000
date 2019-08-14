d87f2a98ddd9eda6cc250ebdd24909b0d7f9867a
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 16:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 }
 
-destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object;
+}
